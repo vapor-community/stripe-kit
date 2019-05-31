6cadf4dd86811967d02407ba0f734e298e691f4a
Andrew Edwards <andrewangeta@gmail.com>
2019-05-31 19:57:23 -0400

    Renamed to StripeKit.

--- Package.swift
+++ Package.swift
@@ -2,16 +2,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "Stripe",
+    name: "StripeKit",
     products: [
-        .library(name: "Stripe", targets: ["Stripe"])
+        .library(name: "StripeKit", targets: ["StripeKit"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/swift-server/swift-nio-http-client.git", .branch("master"))
     ],
     targets: [
-        .target(name: "Stripe", dependencies: ["NIOHTTPClient", "NIOFoundationCompat"]),
-        .testTarget(name: "StripeTests", dependencies: ["NIOHTTPClient", "Stripe"])
+        .target(name: "StripeKit", dependencies: ["NIOHTTPClient", "NIOFoundationCompat"]),
+        .testTarget(name: "StripeKitTests", dependencies: ["NIOHTTPClient", "StripeKit"])
     ]
 )
