8a3cac70ecd3bb84ed1f5dbffafa9a05a7c0c43a
Andrew Edwards <andrewangeta@gmail.com>
2019-12-27 18:44:26 -0600

    Added livemode and metadata to Terminal Location and Reader.

--- Sources/StripeKit/Terminal/Reader/Reader.swift
+++ Sources/StripeKit/Terminal/Reader/Reader.swift
@@ -21,8 +21,12 @@ public struct StripeReader: StripeModel {
     public var ipAddress: String?
     /// Custom label given to the reader for easier identification.
     public var label: String?
+    /// Has the value true if the object exists in live mode or the value false if the object exists in test mode.
+    public var livemode: Bool?
     /// The location identifier of the reader.
     public var location: String?
+    /// Set of key-value pairs that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
+    public var metadata: [String: String]?
     /// Serial number of the reader.
     public var serialNumber: String?
     /// The networking status of the reader.
