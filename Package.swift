7384325e3ba736349f710927fa859d12111a77c8
Andrew Edwards <andrewangeta@gmail.com>
2020-03-07 18:26:26 -0500

    Removed explicit NIO dependency since AsyncHTTPClient pulls it in.

--- Package.swift
+++ Package.swift
@@ -7,11 +7,10 @@ let package = Package(
         .library(name: "StripeKit", targets: ["StripeKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", .exact("1.1.0")),
     ],
     targets: [
-        .target(name: "StripeKit", dependencies: ["AsyncHTTPClient", "NIOFoundationCompat"]),
+        .target(name: "StripeKit", dependencies: ["AsyncHTTPClient"]),
         .testTarget(name: "StripeKitTests", dependencies: ["AsyncHTTPClient", "StripeKit"])
     ]
 )
