54e85e1e654e0c6c159674c3abf8d848a9176760
Andrew Edwards <andrewangeta@gmail.com>
2020-04-19 12:56:37 -0400

    Added Expand Api for fraud.

--- Sources/StripeKit/Fraud/Reviews/ReviewRoutes.swift
+++ Sources/StripeKit/Fraud/Reviews/ReviewRoutes.swift
@@ -12,14 +12,16 @@ public protocol ReviewRoutes {
     /// Approves a `Review` object, closing it and removing it from the list of reviews.
     ///
     /// - Parameter review: The identifier of the review to be approved.
+    /// - Parameter expand: An array of properties to expand.
     /// - Returns: A `StripeReview`.
-    func approve(review: String) -> EventLoopFuture<StripeReview>
+    func approve(review: String, expand: [String]?) -> EventLoopFuture<StripeReview>
     
     /// Retrieves a Review object.
     ///
     /// - Parameter review: The identifier of the review to be retrieved.
+    /// - Parameter expand: An array of properties to expand.
     /// - Returns: A `StripeReview`.
-    func retrieve(review: String) -> EventLoopFuture<StripeReview>
+    func retrieve(review: String, expand: [String]?) -> EventLoopFuture<StripeReview>
     
     /// Returns a list of `Review` objects that have `open` set to `true`. The objects are sorted in descending order by creation date, with the most recently created object appearing first.
     ///
@@ -32,12 +34,12 @@ public protocol ReviewRoutes {
 }
 
 extension ReviewRoutes {
-    func approve(review: String) -> EventLoopFuture<StripeReview> {
-        return approve(review: review)
+    func approve(review: String, expand: [String]? = nil) -> EventLoopFuture<StripeReview> {
+        return approve(review: review, expand: expand)
     }
     
-    func retrieve(review: String) -> EventLoopFuture<StripeReview> {
-        return retrieve(review: review)
+    func retrieve(review: String, expand: [String]? = nil) -> EventLoopFuture<StripeReview> {
+        return retrieve(review: review, expand: expand)
     }
     
     func listAll(filter: [String: Any]? = nil)  -> EventLoopFuture<StripeReviewList> {
@@ -55,12 +57,23 @@ public struct StripeReviewRoutes: ReviewRoutes {
         self.apiHandler = apiHandler
     }    
     
-    public func approve(review: String) -> EventLoopFuture<StripeReview> {
-        return apiHandler.send(method: .POST, path: "\(reviews)\(review)/approve", headers: headers)
+    public func approve(review: String, expand: [String]?) -> EventLoopFuture<StripeReview> {
+        var body: [String: Any] = [:]
+        
+        if let expand = expand {
+            body["expand"] = expand
+        }
+        
+        return apiHandler.send(method: .POST, path: "\(reviews)\(review)/approve", body: .string(body.queryParameters), headers: headers)
     }
     
-    public func retrieve(review: String) -> EventLoopFuture<StripeReview> {
-        return apiHandler.send(method: .GET, path: "\(reviews)\(review)", headers: headers)
+    public func retrieve(review: String, expand: [String]?) -> EventLoopFuture<StripeReview> {
+        var queryParams = ""
+        if let expand = expand {
+            queryParams = ["expand": expand].queryParameters
+        }
+        
+        return apiHandler.send(method: .GET, path: "\(reviews)\(review)", query: queryParams, headers: headers)
     }
     
     public func listAll(filter: [String: Any]?) -> EventLoopFuture<StripeReviewList> {
