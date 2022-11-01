542aebd61267ff7d5068baf860abdcfdeb600576
Andrew Edwards <andrew.edwards@saks.com>
2022-11-01 19:55:26 -0400

    Update BalanceTransaction

--- Sources/StripeKit/Core Resources/Balance Transactions/BalanceTransaction.swift
+++ Sources/StripeKit/Core Resources/Balance Transactions/BalanceTransaction.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 /// The [Balance Transaction Object](https://stripe.com/docs/api/balance/balance_transaction)
-public struct BalanceTransaction: StripeModel {
+public struct BalanceTransaction: Codable {
     /// Unique identifier for the object.
     public var id: String
     /// String representing the object’s type. Objects of the same type share the same value.
@@ -29,18 +29,52 @@ public struct BalanceTransaction: StripeModel {
     /// Fees (in cents) paid for this transaction.
     public var fee: Int?
     /// Detailed breakdown of fees (in cents) paid for this transaction.
-    public var feeDetails: [StripeBalanceTransactionFeeDetails]?
+    public var feeDetails: [BalanceTransactionFeeDetails]?
     /// Net amount of the transaction, in cents.
     public var net: Int?
+    /// [Learn more](https://stripe.com/docs/reports/reporting-categories) about how reporting categories can help you understand balance transactions from an accounting perspective.
+    public var reportingCategory: String?
     /// The Stripe object to which this transaction is related.
     public var source: String?
     /// If the transaction’s net funds are available in the Stripe balance yet. Either `available` or `pending`.
-    public var status: StripeBalanceTransactionStatus?
+    public var status: BalanceTransactionStatus?
     /// Transaction type: `adjustment`, `advance`, `advance_funding`, `application_fee`, `application_fee_refund`, `charge`, `connect_collection_transfer`, `issuing_authorization_hold`, `issuing_authorization_release`, `issuing_transaction`, `payment`, `payment_failure_refund`, `payment_refund`, `payout`, `payout_cancel`, `payout_failure`, `refund`, `refund_failure`, `reserve_transaction`, `reserved_funds`, `stripe_fee`, `stripe_fx_fee`, `tax_fee`, `topup`, `topup_reversal`, `transfer`, `transfer_cancel`, `transfer_failure`, or `transfer_refund`. Learn more about balance transaction types and what they represent.
-    public var type: StripeBalanceTransactionType?
+    public var type: BalanceTransactionType?
+    
+    public init(id: String,
+                object: String,
+                amount: Int? = nil,
+                availableOn: Date? = nil,
+                created: Date,
+                currency: StripeCurrency? = nil,
+                description: String? = nil,
+                exchangeRate: Double? = nil,
+                fee: Int? = nil,
+                feeDetails: [BalanceTransactionFeeDetails]? = nil,
+                net: Int? = nil,
+                reportingCategory: String? = nil,
+                source: String? = nil,
+                status: BalanceTransactionStatus? = nil,
+                type: BalanceTransactionType? = nil) {
+        self.id = id
+        self.object = object
+        self.amount = amount
+        self.availableOn = availableOn
+        self.created = created
+        self.currency = currency
+        self.description = description
+        self.exchangeRate = exchangeRate
+        self.fee = fee
+        self.feeDetails = feeDetails
+        self.net = net
+        self.reportingCategory = reportingCategory
+        self.source = source
+        self.status = status
+        self.type = type
+    }
 }
 
-public struct StripeBalanceTransactionFeeDetails: StripeModel {
+public struct BalanceTransactionFeeDetails: Codable {
     /// Amount of the fee, in cents.
     public var amount: Int?
     /// ID of the Connect application that earned the fee.
@@ -50,21 +84,33 @@ public struct StripeBalanceTransactionFeeDetails: StripeModel {
     /// An arbitrary string attached to the object. Often useful for displaying to users.
     public var description: String?
     /// Type of the fee, one of: `application_fee`, `stripe_fee` or `tax`.
-    public var type: StripeBalanceTransactionFeeDetailsType?
+    public var type: BalanceTransactionFeeDetailsType?
+    
+    public init(amount: Int? = nil,
+                application: String? = nil,
+                currency: StripeCurrency? = nil,
+                description: String? = nil,
+                type: BalanceTransactionFeeDetailsType? = nil) {
+        self.amount = amount
+        self.application = application
+        self.currency = currency
+        self.description = description
+        self.type = type
+    }
 }
 
-public enum StripeBalanceTransactionFeeDetailsType: String, StripeModel {
+public enum BalanceTransactionFeeDetailsType: String, Codable {
     case applicationFee = "application_fee"
     case stripeFee = "stripe_fee"
     case tax
 }
 
-public enum StripeBalanceTransactionStatus: String, StripeModel {
+public enum BalanceTransactionStatus: String, Codable {
     case available
     case pending
 }
 
-public enum StripeBalanceTransactionType: String, StripeModel {
+public enum BalanceTransactionType: String, Codable {
     case adjustment
     case advance
     case advanceFunding = "advance_funding"
@@ -99,9 +145,19 @@ public enum StripeBalanceTransactionType: String, StripeModel {
     case transferRefund = "transfer_refund"
 }
 
-public struct StripeBalanceTransactionList: StripeModel {
+public struct BalanceTransactionList: Codable {
     public var object: String
     public var url: String?
     public var hasMore: Bool?
     public var data: [BalanceTransaction]?
+    
+    public init(object: String,
+                url: String? = nil,
+                hasMore: Bool? = nil,
+                data: [BalanceTransaction]? = nil) {
+        self.object = object
+        self.url = url
+        self.hasMore = hasMore
+        self.data = data
+    }
 }
