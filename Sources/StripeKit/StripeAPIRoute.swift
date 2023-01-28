b412fc77ce2a1c78d37b0a71c7846060a7ae02d9
Andrew Edwards <andrew.edwards@saks.com>
2023-01-28 09:52:22 -0500

    Fixed API route protocol

--- Sources/StripeKit/StripeAPIRoute.swift
+++ Sources/StripeKit/StripeAPIRoute.swift
@@ -12,12 +12,12 @@ public protocol StripeAPIRoute {
     var headers: HTTPHeaders { get set }
     
     /// Headers to send with the request.
-    mutating func addHeaders(_ headers: HTTPHeaders) -> StripeAPIRoute
+    mutating func addHeaders(_ headers: HTTPHeaders) -> Self
 }
 
 
 extension StripeAPIRoute {
-    public mutating func addHeaders(_ headers: HTTPHeaders) -> StripeAPIRoute {
+    public mutating func addHeaders(_ headers: HTTPHeaders) -> Self {
         headers.forEach { self.headers.replaceOrAdd(name: $0.name, value: $0.value) }
         return self
     }
