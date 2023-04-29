c0b8c24f8e91c4960178b7ff30feace6c99c32fc
Andrew Edwards <andrewangeta@gmail.com>
2023-04-29 19:25:31 -0400

    Updated tokens. (#173)
    Co-authored-by: Andrew Edwards <andrew.edwards@saks.com>

--- Sources/StripeKit/Core Resources/Tokens/Token.swift
+++ Sources/StripeKit/Core Resources/Tokens/Token.swift
@@ -8,8 +8,8 @@
 
 import Foundation
 
-/// The [Token Object](https://stripe.com/docs/api/tokens/object).
-public struct StripeToken: Codable {
+/// The [Token Object](https://stripe.com/docs/api/tokens/object) .
+public struct Token: Codable {
     /// Unique identifier for the object.
     public var id: String
     /// String representing the object’s type. Objects of the same type share the same value.
@@ -25,15 +25,36 @@ public struct StripeToken: Codable {
     /// Has the value true if the object exists in live mode or the value false if the object exists in test mode.
     public var livemode: Bool?
     /// Type of the token: `account`, `bank_account`, `card`, or `pii`.
-    public var type: StripeTokenType?
+    public var type: TokenType?
     /// Whether this token has already been used (tokens can be used only once).
     public var used: Bool?
+    
+    public init(id: String,
+                object: String,
+                bankAccount: StripeBankAccount? = nil,
+                card: StripeCard? = nil,
+                clientIp: String? = nil,
+                created: Date,
+                livemode: Bool? = nil,
+                type: TokenType? = nil,
+                used: Bool? = nil) {
+        self.id = id
+        self.object = object
+        self.bankAccount = bankAccount
+        self.card = card
+        self.clientIp = clientIp
+        self.created = created
+        self.livemode = livemode
+        self.type = type
+        self.used = used
+    }
 }
 
-public enum StripeTokenType: String, Codable {
+public enum TokenType: String, Codable {
     case account
     case person
     case bankAccount = "bank_account"
     case card
+    case cvcUpdate = "cvc_update"
     case pii
 }
