ad4f739e036acc08f77a345cf7340b36ade98cb4
Andrew Edwards <andrewangeta@gmail.com>
2023-07-19 18:58:39 -0400

    Updated platform requirements. (#215)

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@ import PackageDescription
 let package = Package(
     name: "stripe-kit",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v12),
+        .iOS(.v15),
+        .tvOS(.v15),
+        .watchOS(.v8)
     ],
     products: [
         .library(name: "StripeKit", targets: ["StripeKit"])
