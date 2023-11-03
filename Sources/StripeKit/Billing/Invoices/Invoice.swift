908208334d78205902107a5c3c5416755136b487
Maxim Anisimov <anisimov.m@gmail.com>
2023-11-03 15:38:38 +0200

    Add a default nil param for SubscriptionDetails init

--- Sources/StripeKit/Billing/Invoices/Invoice.swift
+++ Sources/StripeKit/Billing/Invoices/Invoice.swift
@@ -326,7 +326,7 @@ public struct SubscriptionDetails: Codable {
 	/// Set of key-value pairs that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
 	public var metadata: [String: String]?
 	
-	public init(metadata: [String: String]?) {
+	public init(metadata: [String: String]? = nil) {
 		self.metadata = metadata
 	}
 }
