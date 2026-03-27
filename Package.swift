a208dbfdea37619b77830e396ff09974cca2cb67
DodgyTim <theboss@dodgy.ltd>
2026-03-27 13:08:03 +1300

    fix: Explicit dependency on swift-nio for NIOFoundationCompat

--- Package.swift
+++ Package.swift
@@ -14,12 +14,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.1.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.81.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "5.0.0"),
     ],
     targets: [
         .target(name: "StripeKit", dependencies: [
             .product(name: "AsyncHTTPClient", package: "async-http-client"),
             .product(name: "Crypto", package: "swift-crypto"),
+            .product(name: "NIOFoundationCompat", package: "swift-nio")
         ]),
         .testTarget(name: "StripeKitTests", dependencies: [
             .target(name: "StripeKit")
